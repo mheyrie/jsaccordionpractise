8c769d1881759e6db97f481abf269731708bf8e6
Mheyrie <omota4fidelity@gmail.com>
2024-03-08 02:54:46 +0100

    completed

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const accordion = document.getElementsByClassName('questions-container');
+const accordions = document.querySelectorAll('.questions-container');
 
-for( i = 0; i < accordion.length; i++ ) {
-    accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-    })
+// Adding event listener to toggle active class
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        const answer = this.querySelector('.answer');
+        const question = this.querySelector('.question');
+        if (this.classList.contains('active')) {
+            answer.style.height = answer.scrollHeight + 'px';
+            question.style.fontSize = '15px';
+            
+        } else {
+            answer.style.height = 0;
+            question.style.fontSize = '22px';
+            
+        }
+    });
 }
+
+
